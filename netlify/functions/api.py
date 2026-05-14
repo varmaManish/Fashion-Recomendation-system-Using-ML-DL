9abc630e8e79f6cc823ff66e72d3547d3532d36e
MANISH VARMA <142868993+varmaManish@users.noreply.github.com>
2026-05-14 22:15:07 +0530

    Update api.py

--- netlify/functions/api.py
+++ netlify/functions/api.py
@@ -1,1 +1,8 @@
+from backend.app import create_app
+from serverless_wsgi import handle_request
 
+app = create_app()
+
+def handler(event, context):
+    # This replaces app.run()
+    return handle_request(app, event, context)
